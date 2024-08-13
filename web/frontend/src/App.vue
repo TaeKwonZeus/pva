b875bea8e5bbf0e02ecc28fb8fb0015c3b07ad9a
baka <dyatlovleonid@gmail.com>
2024-08-13 23:19:02 +0300

    a

--- web/frontend/src/App.vue
+++ web/frontend/src/App.vue
@@ -1,14 +1,24 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import { ref } from "vue";
+
+const items = ref([
+  {
+    label: "bruh",
+    icon: "pi pi-home"
+  },
+  {
+    label: "bruh",
+    icon: "pi pi-home"
+  }
+]);
 </script>
 
 <template>
   <header>
-    Header
+    <Menubar :model="items" />
   </header>
 
   <RouterView />
 </template>
 
-<style scoped>
-</style>
--- web/frontend/src/views/HomeView.vue
+++ web/frontend/src/views/HomeView.vue
@@ -2,5 +2,5 @@
 </script>
 
 <template>
-  home
+  Home
 </template>
