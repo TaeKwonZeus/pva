a62e041268d960f9b3c487941e03990f558fae31
baka <dyatlovleonid@gmail.com>
2024-08-13 23:00:40 +0300

    a

--- web/frontend/src/App.vue
+++ web/frontend/src/App.vue
@@ -1,20 +1,10 @@
 <script setup>
 import { RouterView } from 'vue-router'
-import IconField from 'primevue/iconfield';
-import InputIcon from 'primevue/inputicon';
 </script>
 
 <template>
   <header>
-    <Toolbar>
-      <template #start>
-        <IconField>
-          <InputIcon class="pi pi-amazon" />
-          <InputText v-model="a" placeholder="Pablo" />
-        </IconField>
-        asdf
-      </template>
-    </Toolbar>
+    Header
   </header>
 
   <RouterView />
